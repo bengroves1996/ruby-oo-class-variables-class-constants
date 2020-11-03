e1ac3c14a3b4e2b73386916387407524aac3e71a
bengroves <bengroves15@gmail.com>
2020-11-03 02:23:08 -0500

    commit tree c55cde8e1e27bc580c6c6cd2228a66b7ce893c31

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRANDS = [].to_set
+  
   def initialize(brand)
     @brand = brand
+    BRANDS << @brand
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
+
 
 end
